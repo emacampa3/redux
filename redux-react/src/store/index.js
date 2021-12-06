6fbe64c654f0f6f48ab417fad03e5c2ab751eb6e
Ema Čampa <emacampa3@gmail.com>
2021-12-06 17:45:33 +0100

    split the index.js

--- redux-react/src/store/index.js
+++ redux-react/src/store/index.js
@@ -1,30 +1,10 @@
-import { configureStore, createSlice } from "@reduxjs/toolkit"
+import { configureStore } from "@reduxjs/toolkit"
 
-const initialState = { counter: 0, showCounter: true }
-
-const counterSlice = createSlice({
-  name: 'counter',
-  initialState,
-  reducers: {
-    increment(state) {
-      state.counter++
-    },
-    decrement(state) {
-      state.counter--
-    },
-    increase(state, action) {
-      state.counter = state.counter + action.payload
-    },
-    toggle(state) {
-      state.showCounter = !state.showCounter
-    }
-  }
-})
-
+import counterReducer from './counterSlice'
+import authReducer from './authSlice'
 
 const store = configureStore({
-  reducer: counterSlice.reducer
+  reducer: { counter: counterReducer, auth: authReducer } /* reducers are merged together */
 })
 
-export const counterActions = counterSlice.actions
 export default store
