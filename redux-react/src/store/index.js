f40071d55a3e808c44dbd4f69da31ebc69c1575b
Ema Čampa <emacampa3@gmail.com>
2021-12-06 13:35:52 +0100

    new react project using redux

---
+++ redux-react/src/store/index.js
@@ -0,0 +1,26 @@
+import { createStore } from 'redux'
+
+const counterReducer = (state = { counter: 0 }, action) => {
+	if (action.type === "increment") {
+		return {
+			counter: state.counter + 1,
+		}
+	}
+
+	if (action.type == "decrement") {
+		return {
+			counter: state.counter - 1,
+		}
+	}
+
+	return state
+}
+
+const store = createStore(counterReducer) 
+
+const counterSubscriber = () => {
+	const latestState = store.getState()
+	console.log(latestState)
+}
+
+export default store
